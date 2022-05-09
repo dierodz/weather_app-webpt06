ba1bf04969b8ce819491a1f207738e0756620ba2
Diego Rodríguez <diego@uils.la>
2022-05-09 20:11:10 -0300

    fix secure api

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   function onSearch(ciudad) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
     )
       .then((r) => r.json())
       .then((recurso) => {
